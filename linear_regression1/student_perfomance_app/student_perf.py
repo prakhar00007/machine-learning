ad442a4437e3b3c7e51b02637280b6aa6fa08a58
Prakhar <prakharrathore111@gmail.com>
2025-12-26 21:50:11 +0530

    __

--- linear_regression1/student_perfomance_app/student_perf.py
+++ linear_regression1/student_perfomance_app/student_perf.py
@@ -9,7 +9,7 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
 
-uri = "mongodb+srv://prakhar:prakhar@cluster0.ma6xrby.mongodb.net/?appName=Cluster0"
+uri = "mongodb+srv://<username>:<password>@cluster0.ma6xrby.mongodb.net/?appName=Cluster0"
 client = MongoClient(uri, server_api=ServerApi('1'))
 db=client['student']
 collection=db['student_pred']
